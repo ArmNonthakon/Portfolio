511c79b26a9086c7a75af22153331c34ea8d4e70
Arm Nonthakon <nonthakon.t3@gmail.com>
2024-05-25 02:10:25 +0700

    Fix bug

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -6,8 +6,8 @@ const footer = () => {
             <div className='footer'>
 
                 <div className='footer-social'>
-                    <h4>Contact me</h4>
-                    <a href="https://www.facebook.com/people/Nonthakon-Tansamai/pfbid0zB9xYyGJNGkkPzwe2Wfsm6jaP5Yd3zLxwwU3C7hHbtNnHAqVhzWEfPJCjfHCKTSdl/"><img src="/public/facebook (2).png" width="35px" alt="" /></a>
+                    <h3>Contact me</h3>
+                    <a href="https://www.facebook.com/people/Nonthakon-Tansamai/pfbid0zB9xYyGJNGkkPzwe2Wfsm6jaP5Yd3zLxwwU3C7hHbtNnHAqVhzWEfPJCjfHCKTSdl/"><img src="/facebook (2).png" width="35px" alt="" /></a>
                     <a href="https://www.instagram.com/3armm__9"><img src="/instagram (1).png" width="35px" alt="" /></a>
                     <a href="https://www.linkedin.com/in/nonthakon-tansamai-09986430a"><img src="/linkedin.png" width="35px" alt="" /></a>
                     <a href="https://github.com/ArmNonthakon"><img src="/github (1).png" width="35px" alt="" /></a>
