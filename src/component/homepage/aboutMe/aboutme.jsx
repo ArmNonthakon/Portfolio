2f59bec0c937376bc9c926ca11b45463be3a5149
Arm Nonthakon <nonthakon.t3@gmail.com>
2024-05-25 01:13:13 +0700

    Add every page

--- src/component/homepage/aboutMe/aboutme.jsx
+++ src/component/homepage/aboutMe/aboutme.jsx
@@ -17,7 +17,7 @@ function Aboutme() {
                         <h1>Nonthakon Tansamai (Arm)</h1>
                         <h1>Student</h1>
                     </div>
-                    <button>CONTACT ME</button>
+                    <a href="/contact"><button>CONTACT ME</button></a>
                 </div>
             </div>
             <div className='content-text'>
