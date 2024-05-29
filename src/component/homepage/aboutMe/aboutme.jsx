2ddd2e47f3cb00bd4704679509aa2f61944cc3b9
Arm Nonthakon <nonthakon.t3@gmail.com>
2024-05-29 17:01:44 +0700

    Add responsive

--- src/component/homepage/aboutMe/aboutme.jsx
+++ src/component/homepage/aboutMe/aboutme.jsx
@@ -8,10 +8,7 @@ function Aboutme() {
                     <div className='Pic-personal'></div>
                     <div className='pic-deco'></div>
                     <div className='pic-deco'></div>
-
-
                 </div>
-
                 <div className='sec-text'>
                     <div className='sec-text-name'>
                         <h1>Nonthakon Tansamai (Arm)</h1>
@@ -21,9 +18,7 @@ function Aboutme() {
                 </div>
             </div>
             <div className='content-text'>
-                <h1 className='about-me-animation'>
-
-                </h1>
+                <h1 className='about-me-animation'></h1>
                 <p><span style={{ marginLeft: "50px" }}>        </span>I am studying computer science at Silpakorn University. I'm a third-year student. I used to do many projects such as website e-commerce, simple web chat, machine learning for classifying fake news</p>
 
             </div>
@@ -43,16 +38,19 @@ function Aboutme() {
                     </div>
                 </div>
                 <div className='education-road'>
+                    
                     <p>2021 - Present
                         Silpakorn University
                         bachelor of science in computer science degree
                         GPA : 2.55</p>
+                        <br />
                     <p>
                         2015- 2020
                         Sirirattanathorn School
                         English-Mathematics Program
                         GPA : 2.8
                     </p>
+                    <br />
                     <p>2009 - 2014
                         Bannongbon School</p>
                 </div>
--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -1,16 +1,42 @@
+import { useState } from "react"
 import "./navbar.css"
 
 function Navbar() {
+    const [stateList,SetStateList] = useState(false)
+    const showList = ()=>{
+        if (stateList == false){
+            document.getElementById('showListMobile').style.display = "flex"
+            SetStateList(true)
+        }
+        else {
+            document.getElementById('showListMobile').style.display = "none"
+            SetStateList(false)
+        }
+        
+    }
     return (
         <>
             <nav>
-                <div className="pic-block">
+                <div className="mobile">
+                    <div className="pic-block">
+                    </div>
+                    <img src="/list.png" width="35px" height="35px" onClick={showList} alt="" />
                 </div>
-                
-                <a href="/">ABOUT ME</a>
-                <a href="/skill">SKILL</a>
-                <a href="/project">PROJECT</a>
-                <a href="/contact">CONTACT ME</a>
+                <div className="desktop">
+                    <div className="pic-block">
+                    </div>
+                </div>
+                <div id="showListMobile" className="mobileItem">
+                    <a href="/">ABOUT ME</a>
+                    <a href="/skill">SKILL</a>
+                    <a href="/project">PROJECT</a>
+                    <a href="/contact">CONTACT ME</a>
+                </div>
+                <a className="desktopItem" href="/">ABOUT ME</a>
+                <a className="desktopItem" href="/skill">SKILL</a>
+                <a className="desktopItem" href="/project">PROJECT</a>
+                <a className="desktopItem" href="/contact">CONTACT ME</a>
+
             </nav>
         </>)
 }
